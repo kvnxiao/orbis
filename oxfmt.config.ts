37d3b42107ca38ebb294547e488df59afd7ab41e
Kevin Xiao <xlzqwerty1@gmail.com>
2026-09-09 01:35:09 -0400

    Add oxfmt config

---
+++ oxfmt.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  sortImports: true,
+  sortTailwindcss: true,
+  sortPackageJson: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+  ignorePatterns: ["pnpm-lock.yaml", "LICENSE", "**/LICENSE", "node_modules/**", ".artifacts/**"],
+  jsdoc: {
+    addDefaultToDescription: true,
+    bracketSpacing: false,
+    capitalizeDescriptions: true,
+    commentLineStrategy: "singleLine",
+    preferCodeFences: true,
+  },
+  jsxSingleQuote: false,
+});
